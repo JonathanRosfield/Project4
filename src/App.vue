6729e35aa971312ac547a6a0fddbee2397d56824
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-09-21 01:17:48 -0600

    ContactForm component finished

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script setup>
+import {ref} from 'vue';
 import Header from './components/Header.vue';
+import ContactForm from './components/ContactForm.vue';
 
+const contacts = ref([]);
 
+// New contact addition
+const handleContactAdded = (contact) => {
+  contacts.value.push(contact);
+};
 
 </script>
 
 <template>
 <Header></Header>
-
+<div class="container">
+    <ContactForm @contactAdded="handleContactAdded" />
+    
+  </div>
 
 
 
--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,7 +1,26 @@
 <script setup>
+import { ref, defineEmits } from 'vue';
 
+// Define emit for an added contact
+const emit = defineEmits(['contactAdded']);
 
+// Define reactive properties
+const contactName = ref('');
+const contactPhone = ref('');
 
+// Form submission
+const onSubmit = () => {
+    if (contactName.value && contactPhone.value){
+        emit('contactAdded',{
+            name: contactName.value,
+            phone: contactPhone.value,
+        });
+
+      // Clear the form inputs
+      contactName.value = '';
+      contactPhone.value = '';
+    }
+};
 
 
 </script>
@@ -11,14 +30,16 @@
 <div id="contact-form">
     <h2>Add a New Contact</h2>
     <form @submit.prevent="onSubmit">
-        <div class="form-control"></div>
+        <div class="form-control">
             <label for="contactName">Name:</label>
             <input v-model="contactName" type="text" id="contactName" required />
         </div>
-        
-
-
-
-
+        <div class="form-control">
+            <label for="contactPhone">Phone Number:</label>
+            <input v-model="contactPhone" type="tell" id = "contactPhone" required />
+        </div>
+        <button type="submit">Add Contact</button>
+    </form>
+</div>
 
 </template>
