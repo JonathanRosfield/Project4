e5ee08c2bf754f5bdb999dfd3743a002de3d8c3c
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-09-20 22:30:26 -0600

    Initial commit with all the files

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+
+
+
+
+</script>
+
+
+<template>
+<div id="contact-form">
+    <h2>Add a New Contact</h2>
+    <form @submit.prevent="onSubmit">
+        <div class="form-control"></div>
+            <label for="contactName">Name:</label>
+            <input v-model="contactName" type="text" id="contactName" required />
+        </div>
+        
+
+
+
+
+
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+
+
+
+</script>
+
+
+
+<template>
+
+<h4>Create your Contact List!</h4>
+
+
+</template>
